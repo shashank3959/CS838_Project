f4517f6a96c6ae20d61b49789f022af86653d07b
Shashank Verma <shashank3959@gmail.com>
2019-04-04 22:13:36 -0500

    First code commit
    Signed-off-by: Shashank Verma <shashank3959@gmail.com>

---
+++ code/main.py
@@ -0,0 +1,41 @@
+import argparse
+import os
+import time
+
+
+# Parsing command line arguments
+print("Process %s, running on %s: starting (%s)" % (
+        os.getpid(), os.uname()[1], time.asctime()))
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--data-train", type=str, default='',
+        help="training data json")
+parser.add_argument("--data-val", type=str, default='',
+        help="validation data json")
+parser.add_argument("--exp-dir", type=str, default="",
+        help="directory to dump experiments")
+parser.add_argument("--resume", action="store_true", dest="resume",
+        help="load from exp_dir if True")
+parser.add_argument("--optim", type=str, default="sgd",
+        help="training optimizer", choices=["sgd", "adam"])
+parser.add_argument('-b', '--batch-size', default=100, type=int,
+    metavar='N', help='mini-batch size (default: 100)')
+parser.add_argument('--lr', '--learning-rate', default=0.001, type=float,
+    metavar='LR', help='initial learning rate')
+parser.add_argument('--lr-decay', default=40, type=int, metavar='LRDECAY',
+    help='Divide the learning rate by 10 every lr_decay epochs')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+    help='momentum')
+parser.add_argument('--weight-decay', '--wd', default=5e-7, type=float,
+    metavar='W', help='weight decay (default: 1e-4)')
+parser.add_argument("--n_epochs", type=int, default=100,
+        help="number of maximum training epochs")
+parser.add_argument("--n_print_steps", type=int, default=100,
+        help="number of steps to print statistics")
+parser.add_argument("--image-model", type=str, default="VGG16",
+        help="image model architecture", choices=["VGG16"])
+parser.add_argument("--pretrained-image-model", action="store_true",
+    dest="pretrained_image_model", help="Use an image network pretrained on ImageNet")
+parser.add_argument("--margin", type=float, default=1.0, help="Margin paramater for triplet loss")
+parser.add_argument("--simtype", type=str, default="MISA",
+        help="matchmap similarity function", choices=["SISA", "MISA", "SIMA"])

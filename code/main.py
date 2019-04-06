2a8568b05dcdf38aae182275bef54b0931629999
Shashank Verma <shashank3959@gmail.com>
2019-04-06 00:00:24 -0500

    Add dataloader and vocabulary builder
    Signed-off-by: Shashank Verma <shashank3959@gmail.com>

--- code/main.py
+++ code/main.py
@@ -1,41 +1,62 @@
 import argparse
 import os
 import time
-
-
-# Parsing command line arguments
-print("Process %s, running on %s: starting (%s)" % (
-        os.getpid(), os.uname()[1], time.asctime()))
+from torchvision import transforms
+from dataloader import get_loader
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("--data-train", type=str, default='',
-        help="training data json")
+                    help="training data json")
 parser.add_argument("--data-val", type=str, default='',
-        help="validation data json")
+                    help="validation data json")
 parser.add_argument("--exp-dir", type=str, default="",
-        help="directory to dump experiments")
+                    help="directory to dump experiments")
 parser.add_argument("--resume", action="store_true", dest="resume",
-        help="load from exp_dir if True")
+                    help="load from exp_dir if True")
 parser.add_argument("--optim", type=str, default="sgd",
-        help="training optimizer", choices=["sgd", "adam"])
+                    help="training optimizer", choices=["sgd", "adam"])
 parser.add_argument('-b', '--batch-size', default=100, type=int,
-    metavar='N', help='mini-batch size (default: 100)')
+                    metavar='N', help='mini-batch size (default: 100)')
 parser.add_argument('--lr', '--learning-rate', default=0.001, type=float,
-    metavar='LR', help='initial learning rate')
+                    metavar='LR', help='initial learning rate')
 parser.add_argument('--lr-decay', default=40, type=int, metavar='LRDECAY',
-    help='Divide the learning rate by 10 every lr_decay epochs')
+                    help='Divide the learning rate by 10 every lr_decay epochs')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
-    help='momentum')
+                    help='momentum')
 parser.add_argument('--weight-decay', '--wd', default=5e-7, type=float,
-    metavar='W', help='weight decay (default: 1e-4)')
+                    metavar='W', help='weight decay (default: 1e-4)')
 parser.add_argument("--n_epochs", type=int, default=100,
-        help="number of maximum training epochs")
+                    help="number of maximum training epochs")
 parser.add_argument("--n_print_steps", type=int, default=100,
-        help="number of steps to print statistics")
+                    help="number of steps to print statistics")
 parser.add_argument("--image-model", type=str, default="VGG16",
-        help="image model architecture", choices=["VGG16"])
+                    help="image model architecture", choices=["VGG16"])
 parser.add_argument("--pretrained-image-model", action="store_true",
-    dest="pretrained_image_model", help="Use an image network pretrained on ImageNet")
-parser.add_argument("--margin", type=float, default=1.0, help="Margin paramater for triplet loss")
-parser.add_argument("--simtype", type=str, default="MISA",
-        help="matchmap similarity function", choices=["SISA", "MISA", "SIMA"])
+                    dest="pretrained_image_model", help="Use an image network pretrained on ImageNet")
+parser.add_argument("--margin", type=float, default=1.0, help="Margin parameter for triplet loss")
+parser.add_argument("--crop_size", type=int, default=224 , help="size for randomly cropping images")
+
+
+# resume from a checkpoint? - add code here
+def main(args):
+    # Parsing command line arguments
+    print("Process %s, running on %s: starting (%s)" % (
+        os.getpid(), os.uname()[1], time.asctime()))
+
+    # data augmentation: normalization for the pretrained resnet
+    transform = transforms.Compose([
+        transforms.RandomCrop(args.crop_size),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406),
+                             (0.229, 0.224, 0.225))])
+
+    # Build data loader
+    data_loader = get_loader(args.image_dir, args.caption_path, vocab,
+                             transform, args.batch_size,
+                             shuffle=True, num_workers=args.num_workers)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args)

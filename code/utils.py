c39cc93aa2d4f920e0d4db83acc8ee25001bdefe
Deepan Das <deepandas11@gmail.com>
2019-04-07 02:49:23 -0500

    Loss Function added
    Signed-off-by: Deepan Das <deepandas11@gmail.com>

--- code/utils.py
+++ code/utils.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 
 def matchmap_generate(image, text):
@@ -30,3 +31,56 @@ def compute_similarity_score(matchmap, score_type):
         return max_text.mean()
     else:
         raise ValueError
+
+
+def custom_loss(image_output, text_output, score_type = 'Max_Img', margin = 1):
+
+    assert(image_output.dim() == 4)
+    assert(text_output.dim() == 3)
+
+    n_imgs = image_output.size(0)
+
+    loss = torch.zeros(1, device=image_output.device, requires_grad=True)
+
+    for i in range(n_imgs):
+        img_impostor_index = i
+        text_impostor_index = i
+
+        # Create impostor index
+        while img_impostor_index == i:
+            img_impostor_index = np.random.randint(0,n_imgs)
+        while text_impostor_index == i:
+            text_impostor_index = np.random.randint(0,n_imgs)
+
+        anchor_score = compute_similarity_score(matchmap_generate(image_output[i], text_output[i]), score_type)
+        image_imp_score = compute_similarity_score(matchmap_generate(image_output[img_impostor_index], text_output[i]), score_type)
+        text_imp_score = compute_similarity_score(matchmap_generate(image_output[i], text_output[text_impostor_index]), score_type)
+
+        text_imp = text_imp_score - anchor_score + margin
+        image_imp = image_imp_score - anchor_score + margin
+
+        if (text_imp.data > 0).all():
+            loss = loss + text_imp
+
+        if (image_imp.data > 0).all():
+            loss = loss + image_imp
+
+    loss = loss/n_imgs
+    return loss
+
+
+class AverageMeter(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count

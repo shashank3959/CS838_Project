b736e2d7bdcebfc590378cadea6994ba6d587a9d
Deepan Das <deepandas11@gmail.com>
2019-04-13 17:12:24 -0500

    Enhanced API for matchmap visualization
    Signed-off-by: Deepan Das <deepandas11@gmail.com>

--- code/matchmap_visualize.py
+++ code/matchmap_visualize.py
@@ -18,8 +18,36 @@ transform = transforms.Compose([
         transforms.Normalize((0.485, 0.456, 0.406),
                              (0.229, 0.224, 0.225))])
 
-def rgb2gray(rgb):
-    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
+
+
+def caption_list_gen(caption):
+    caption_list = []
+    for i in range(len(caption[0])):
+        new_caption = []
+        for j in range(len(caption)):
+            new_caption.append(caption[j][i])
+        caption_list.append(new_caption)
+    return caption_list
+
+
+def get_data(batch_size, fetch_mode='retrieval'):
+    data_loader_val = get_loader(transform=transform,
+                                 mode='val',
+                                 batch_size=5,
+                                 vocab_from_file=True,
+                                 fetch_mode='retrieval')
+
+    indices = data_loader_val.dataset.get_indices()
+    new_sampler = data.sampler.SubsetRandomSampler(indices=indices)
+    data_loader_val.batch_sampler.sampler = new_sampler
+
+    for batch in data_loader_val:
+        image_tensor, caption_glove_tensor, captions = batch[0], batch[1], batch[2]
+        break
+
+    caption_list = caption_list_gen(captions)
+
+    return image_tensor, caption_glove_tensor, caption_list
 
 
 def load_model(model_path = 'model_best.pth.tar', map_location='cpu'):
@@ -34,25 +62,72 @@ def load_model(model_path = 'model_best.pth.tar', map_location='cpu'):
 
 
 def gen_matchmap(image_model, caption_model, image_tensor, caption_tensor):
+    # Generates a stack of matchmaps of length batch size
     image_op = image_model(image_tensor)
     caption_op = caption_model(caption_tensor)
 
-    matchmap = matchmap_generate(image_op[0], caption_op[0])
-    mm = matchmap.detach().numpy()
+    n_imgs = image_op.size(0)
+    matchmap_list = []
 
-    return mm
+    for i in range(n_imgs):
+        matchmap = matchmap_generate(image_op[i], caption_op[i])
+        mm = matchmap.detach().numpy()
+        matchmap_list.append(mm)
+
+    return matchmap_list
 
 
-def gen_masks(image_model, caption_model, image_tensor, caption_tensor):
-    matchmap = gen_matchmap(image_model, caption_model, image_tensor, caption_tensor)
-    I = image_tensor[0].permute(1, 2, 0)
-    I = I.numpy()
-    Img = rgb2gray(I)
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
+
+
+def tensor2img(tensor_image):
+    img = tensor_image.permute(1,2,0)
+    color_img = img.numpy()
+    bw_img = rgb2gray(color_img)
+
+    return color_img, bw_img
+
+
+def gen_masks(image_model, caption_model, image_tensor,
+              caption_tensor, caption_list,
+              index = 0):
+    matchmap_list = gen_matchmap(image_model, caption_model, image_tensor, caption_tensor)
+    n_imgs = len(matchmap_list)
+
+    assert n_imgs >= index
+
+    target_image = image_tensor[index-1]
+    target_caption = caption_list[index-1]
+    target_matchmap = matchmap_list[index-1]
+
+    color_img, bw_img = tensor2img(target_image)
+
     mask_list = []
-    for mmap in matchmap:
+    for mmap in target_matchmap:
         mask = cv2.resize(mmap, dsize=(224, 224))
         mask_list.append(mask)
 
     mask_list = np.stack(mask_list, axis=0)
 
-    return Img, I, mask_list
+    return color_img, bw_img, mask_list, target_caption
+
+
+def show_me(color_img, bw_img, mask_list, caption):
+    plt.imshow(color_img)
+    plt.title('Original Image')
+    plt.show()
+
+    fig = plt.figure(figsize=(50, 200), facecolor='white')
+
+    columns = len(mask_list) + 1
+    rows = 1
+
+    for i in range(1, len(mask_list) + 1):
+        mask = cv2.resize(mask_list[i - 1], dsize=(224, 224))
+        fig.add_subplot(rows, columns, i)
+        plt.imshow(bw_img)
+        plt.imshow(mask, cmap='jet', alpha=0.4)
+        plt.title(caption[i - 1])
+
+    plt.show()

2a158064e5f97afdea6456473da61fbeeb773fe6
Deepan Das <deepandas11@gmail.com>
2019-04-13 05:47:15 -0500

    API for main.ipynb
    Signed-off-by: Deepan Das <deepandas11@gmail.com>

--- code/matchmap_visualize.py
+++ code/matchmap_visualize.py
@@ -1,61 +1,16 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as f
-from torch.autograd import Variable
 from torchvision import transforms
 import torch.utils.data as data
-
-
-import argparse
+import cv2
 import numpy as np
-import math
-import os
 import matplotlib.pyplot as plt
-
 from utils import *
 from models import *
 from data_loader import get_loader
 
-parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument("--resume_filename", type=str, default="",
-                    help="Path to checkpoint file")
-parser.add_argument("--cnn_model", type=str, default="vgg",
-                    help="CNN Architecture used")
-args = parser.parse_args()
-
-caption_model = LSTMBranch(batch_size = 64)
-
-if args.cnn_model == 'vgg':
-    image_model = VGG19(pretrained=True)
-    print('VGG Model Loaded')
-else:
-    image_model = ResNet50(pretrained=True)
-    print('ResNet model Loaded')
-
-
-
-if args.resume_filename:
-    if os.path.isfile(args.resume_filename):
-        print("=> Loading Checkpoint '{}'".format(args.resume_filename))
-        checkpoint = torch.load(args.resume_filename)
-        start_epoch = checkpoint['epoch']
-        best_loss = checkpoint['best_loss']
-        image_model.load_state_dict(checkpoint['image_model'])
-        caption_model.load_state_dict(checkpoint['caption_model'])
-        print("Loaded checkpoint '{}' (epoch {})".format(args.resume_filename, checkpoint['epoch']))
-
-
-    else:
-        print(" => No checkpoint found at '{}'".format(args.resume_filename))
-
-else:
-    print("Checkpoint file not provided!")
-
-if torch.cuda.is_available() and args.use_gpu == True:
-    print("Loading models onto GPU to accelerate training")
-    image_model = image_model.cuda()
-    caption_model = caption_model.cuda()
+image_model = VGG19(pretrained=True)
+caption_model = LSTMBranch()
 
 transform = transforms.Compose([
         transforms.Resize((224, 224)),
@@ -63,9 +18,41 @@ transform = transforms.Compose([
         transforms.Normalize((0.485, 0.456, 0.406),
                              (0.229, 0.224, 0.225))])
 
-data_loader_val = get_loader(transform=transform,
-                             mode='val',
-                             batch_size=args.batch_size,
-                             vocab_from_file=True)
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
 
 
+def load_model(model_path = 'model_best.pth.tar', map_location='cpu'):
+    image_model = VGG19(pretrained=True)
+    checkpoint = torch.load(model_path, map_location)
+    start_epoch = checkpoint['epoch']
+    best_loss = checkpoint['best_loss']
+    image_model.load_state_dict(checkpoint['image_model'])
+    caption_model.load_state_dict(checkpoint['caption_model'])
+    print("Loaded checkpoint '{}' (epoch {})".format(model_path, checkpoint['epoch']))
+    return image_model, caption_model
+
+
+def gen_matchmap(image_model, caption_model, image_tensor, caption_tensor):
+    image_op = image_model(image_tensor)
+    caption_op = caption_model(caption_tensor)
+
+    matchmap = matchmap_generate(image_op[0], caption_op[0])
+    mm = matchmap.detach().numpy()
+
+    return mm
+
+
+def gen_masks(image_model, caption_model, image_tensor, caption_tensor):
+    matchmap = gen_matchmap(image_model, caption_model, image_tensor, caption_tensor)
+    I = image_tensor[0].permute(1, 2, 0)
+    I = I.numpy()
+    Img = rgb2gray(I)
+    mask_list = []
+    for mmap in matchmap:
+        mask = cv2.resize(mmap, dsize=(224, 224))
+        mask_list.append(mask)
+
+    mask_list = np.stack(mask_list, axis=0)
+
+    return Img, I, mask_list

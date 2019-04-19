eb27f6cb37d240ad75f9828bc7a1ff17d099e8a6
Deepan Das <deepandas11@gmail.com>
2019-04-19 13:59:27 -0500

    cleaned argument use cases
    Signed-off-by: Deepan Das <deepandas11@gmail.com>

--- code/main.py
+++ code/main.py
@@ -18,40 +18,60 @@ from tensorboardX import SummaryWriter
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 
-parser.add_argument("--optim", type=str, default="sgd",
-                    help="training optimizer", choices=["sgd", "adam"])
-parser.add_argument('-b', '--batch-size', default=64, type=int,
-                    metavar='N', help='mini-batch size (default: 100)')
 parser.add_argument('--lr', '--learning-rate', default=0.001, type=float,
                     metavar='LR', help='initial learning rate')
-parser.add_argument('--lr-decay', default=10, type=int, metavar='LRDECAY',
+
+parser.add_argument('--lr_decay', default=10, type=int, metavar='LRDECAY',
                     help='Divide the learning rate by 10 every lr_decay epochs')
-parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
-                    help='momentum')
-parser.add_argument('--weight-decay', '--wd', default=1e-7, type=float,
-                    metavar='W', help='weight decay (default: 1e-4)')
+
 parser.add_argument("--n_epochs", type=int, default=10,
                     help="number of maximum training epochs. -1 default refers to infinite")
+
 parser.add_argument("--margin", type=float, default=0.1,
                     help="Margin parameter for triplet loss")
-parser.add_argument("--crop_size", type=int, default=224,
-                    help="size for randomly cropping images")
+
 parser.add_argument("--use_gpu", type=bool, default=True,
                     help="Use GPU to accelerate training")
-parser.add_argument('--name', default='Two_Branch_Image_Sentence', type=str,
-                    help='name of experiment')
-parser.add_argument('--minimum_gain', type=float, default=5e-1, metavar='N',
-                    help='minimum performance gain for a model to be considered better. (default: 5e-1)')
-parser.add_argument('--no_gain_stop', type=int, default=10, metavar='N',
-                    help='number of epochs used to perform early stopping based on validation performance (default: 10)')
+
+parser.add_argument('--loss_type', default='triplet', type=str,
+                    help='kind of loss function to be implemented')
+
 parser.add_argument('--score_type', type=str, default='Avg_Both',
                     help='Metric used to compute score.')
+
 parser.add_argument("--sampler", type=str, default='hard',
                     help="Sampling strategy")
+
+parser.add_argument("--optim", type=str, default="sgd",
+                    help="training optimizer", choices=["sgd", "adam"])
+
+parser.add_argument('-b', '--batch-size', default=64, type=int,
+                    metavar='N', help='mini-batch size (default: 100)')
+
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+
+parser.add_argument('--weight_decay', '--wd', default=1e-7, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)')
+
+parser.add_argument("--crop_size", type=int, default=224,
+                    help="size for randomly cropping images")
+
+parser.add_argument('--name', default='Two_Branch_Image_Sentence', type=str,
+                    help='name of experiment')
+
+parser.add_argument('--minimum_gain', type=float, default=5e-1, metavar='N',
+                    help='minimum performance gain for a model to be considered better. (default: 5e-1)')
+
+parser.add_argument('--no_gain_stop', type=int, default=10, metavar='N',
+                    help='number of epochs used to perform early stopping based on validation performance (default: 10)')
+
 parser.add_argument("--cnn_model", type=str, default='vgg',
                     help="CNN Model")
+
 parser.add_argument('--resume', default='', type=str,
                     help='path to latest checkpoint of best model (default: none)')
+
 
 
 def main(args):
@@ -103,7 +123,16 @@ def main(args):
     total_train_step = math.ceil(
         len(data_loader_train.dataset.caption_lengths) / data_loader_train.batch_sampler.batch_size)
     # print("Total number of training steps are :", total_train_step)
-    print("Sampling strategy -> ", args.sampler)
+
+    print("========================================================")
+    print("Loss Type: ", args.loss_type)
+    if args.loss_type == 'triplet':
+        print("Sampling strategy: ", args.sampler)
+        print("Margin for triplet loss: ", args.margin)
+    print("Learning Rate: ", args.lr)
+    print("Score Type for similarity: ", args.score_type)
+    print("========================================================")
+
     epoch = start_epoch
     best_epoch = start_epoch
 
@@ -114,13 +143,13 @@ def main(args):
         print("Epoch: %d Training starting" % epoch)
         print("Learning rate : ", get_lr(optimizer))
         train_loss = train(data_loader_train, data_loader_val, image_model,
-                              caption_model, optimizer, epoch, args.score_type,
-                              args.sampler, args.margin, total_train_step,
-                              args.batch_size, args.use_gpu)
+                              caption_model, args.loss_type, optimizer, epoch,
+                              args.score_type, args.sampler, args.margin,
+                              total_train_step, args.batch_size, args.use_gpu)
         print('---------------------------------------------------------')
         print("Epoch: %d Validation starting" % epoch)
         val_loss = validate(caption_model, image_model, data_loader_val,
-                            epoch, args.score_type, args.sampler,
+                            epoch, args.loss_type, args.score_type, args.sampler,
                             args.margin, args.use_gpu)
         print("Epoch: ", epoch)
         print("Training Loss: ", float(train_loss.data))
@@ -135,7 +164,7 @@ def main(args):
             'caption_model': caption_model.state_dict()
         }, val_loss < best_loss)
         print("Saved Checkpoint!")
-        if (val_loss + args.minimum_gain) < best_loss:
+        if (val_loss) < best_loss:
             best_epoch = epoch
             best_loss = val_loss
 
@@ -143,15 +172,30 @@ def main(args):
 
     print("Back to main")
     resume_filename = 'runs/%s/' % (args.name) + 'model_best.pth.tar'
-    epoch, best_loss1 = load_checkpoint(image_model, caption_model, args.resume)
-    val_loss1 = validate(caption_model, image_model,
-                         data_loader_val, epoch, args.score_type,
-                         args.sampler, args.margin, args.use_gpu)
-    print("========================================================")
-    print("========================================================")
-    print("Final Loss : ", float(val_loss1.data))
-    print("========================================================")
-    print("========================================================")
+    if os.path.exists(resume_filename):
+
+        epoch, best_loss1 = load_checkpoint(image_model, caption_model, args.resume)
+        val_loss1 = validate(caption_model, image_model, data_loader_val,
+                                epoch, args.loss_type, args.score_type, args.sampler,
+                                args.margin, args.use_gpu)
+        print("========================================================")
+        print("========================================================")
+        print("Final Loss : ", float(val_loss1.data))
+        print("========================================================")
+        print("========================================================")
+
+    else:
+        resume_filename = 'runs/%s/' % (args.name) + 'checkpoint.pth.tar'
+        print("Using last run epoch.")
+        epoch, best_loss1 = load_checkpoint(image_model, caption_model, args.resume)
+        val_loss1 = validate(caption_model, image_model, data_loader_val,
+                             epoch, args.loss_type, args.score_type, args.sampler,
+                             args.margin, args.use_gpu)
+        print("========================================================")
+        print("========================================================")
+        print("Final Loss : ", float(val_loss1.data))
+        print("========================================================")
+        print("========================================================")
 
 
 def get_lr(optimizer):
@@ -174,7 +218,10 @@ def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
 
 def load_checkpoint(image_model, caption_model, resume_filename):
     start_epoch = 1
-    best_loss = 2 * args.margin
+    if args.loss_type == 'triplet':
+        best_loss = 2 * args.margin
+    else:
+        best_loss = 1.0
 
     if resume_filename:
         if os.path.isfile(resume_filename):
